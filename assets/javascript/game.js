417c6d7603c91401e33697e7fb6d771d82a124bb
Muscle Everest <danza_seppia@yahoo.com>
2018-07-25 14:16:46 -0500

    final changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,103 @@
+var seaAnimals = ["pilot whale", "plankton", "hammerhead shark", "ctenophore", "sea sponge", "barnacle", "copepod", "starfish", "manta ray", "bottlenose dolphin", "humboldt squid", "spider crab", "dogfish", "leatherback turtle", "octopus", "marlin", "remora", "gerald"];
+
+var winCount = 1;
+var lossCount = 1;
+var guessesLeft = 6;
+var oldGuesses = [];
+
+
+var computerChoice = seaAnimals[Math.floor(Math.random() * seaAnimals.length)];
+
+var blankWord = [];
+
+for (i = 0; i < computerChoice.length; i++) {
+    if (computerChoice[i] === " ") {
+        blankWord.push("&nbsp");
+    } else {
+        blankWord.push("_");
+    };
+};
+
+document.getElementById("current-word").innerHTML = blankWord.join("");
+
+function newGame() {
+    document.getElementById("press-key").style.visibility = "visible";
+    computerChoice = seaAnimals[Math.floor(Math.random() * seaAnimals.length)];
+
+    blankWord = [];
+        
+        for (i = 0; i < computerChoice.length; i++) {
+            if (computerChoice[i] === " ") {
+                blankWord.push("&nbsp");
+            } else {
+                blankWord.push("_");
+            };
+        };
+        
+        document.getElementById("current-word").innerHTML = blankWord.join("");
+        oldGuesses = [];
+        document.getElementById("old-guesses").innerHTML = oldGuesses.join("");
+        guessesLeft = 6;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+        document.getElementById("head").style.visibility = "hidden";
+        document.getElementById("right-arm").style.visibility = "hidden";
+        document.getElementById("left-arm").style.visibility = "hidden";
+        document.getElementById("his-body").style.visibility = "hidden";
+        document.getElementById("right-leg").style.visibility = "hidden";
+        document.getElementById("left-leg").style.visibility = "hidden";
+}
+
+document.onkeyup = function(event) {
+
+    document.getElementById("press-key").style.visibility = "hidden";
+
+    var userGuess = event.key;
+
+    for (var i = 0; i < computerChoice.length; i++) {
+        if (computerChoice[i] === userGuess) {
+            blankWord.splice(i, 1, userGuess);
+            document.getElementById("current-word").innerHTML = blankWord.join("");
+        } 
+    }
+
+    if (computerChoice.indexOf(userGuess) === -1) {
+        guessesLeft = guessesLeft - 1;
+        document.getElementById("guesses-left").innerHTML = guessesLeft;
+        if (oldGuesses.indexOf(userGuess) === -1) {
+            oldGuesses.push(userGuess);
+            document.getElementById("old-guesses").innerHTML = oldGuesses.join("");
+        };
+        if (guessesLeft === 5) {
+        document.getElementById("head").style.visibility = "visible";
+        } else if (guessesLeft === 4) {
+            document.getElementById("his-body").style.visibility = "visible";
+        } else if (guessesLeft === 3) {
+            document.getElementById("right-arm").style.visibility = "visible";
+        } else if (guessesLeft === 2) {
+            document.getElementById("left-arm").style.visibility = "visible";
+        } else if (guessesLeft === 1) {
+            document.getElementById("right-leg").style.visibility = "visible";
+        } else if (guessesLeft === 0) {
+            document.getElementById("left-leg").style.visibility = "visible";
+        };
+    }
+
+    if (guessesLeft === 0) {
+        document.getElementById("loss-count").innerHTML = lossCount;
+        lossCount = lossCount + 1;
+        blankWord = computerChoice;
+        document.getElementById("current-word").innerHTML = blankWord;
+        alert("You lost!");
+        setTimeout(newGame, 5000);
+    }   
+
+    if (blankWord.indexOf("_") === -1 && guessesLeft !== 0) {
+        document.getElementById("win-count").innerHTML = winCount;
+        winCount = winCount + 1;
+        alert("You win!");
+        setTimeout(newGame, 5000);
+    }
+
+
+    }
+
